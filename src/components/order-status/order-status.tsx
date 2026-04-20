ae3d618d7d31b1423a13ade02be2eecd591c8f80
Недбай Иван Дмитриевич <i.nedbai@MacBook-Pro-de-Nedbaj.local>
2026-04-20 21:12:35 +0300

    review

--- src/components/order-status/order-status.tsx
+++ src/components/order-status/order-status.tsx
@@ -2,14 +2,17 @@ import React, { FC } from 'react';
 import { OrderStatusProps } from './type';
 import { OrderStatusUI } from '@ui';
 
+// Сопоставление статусов заказа с текстом для отображения
 const statusText: { [key: string]: string } = {
   pending: 'Готовится',
   done: 'Выполнен',
   created: 'Создан'
 };
 
+// Компонент для отображения статуса заказа с цветом и текстом
 export const OrderStatus: FC<OrderStatusProps> = ({ status }) => {
   let textStyle = '';
+  // Определяем цвет текста в зависимости от статуса
   switch (status) {
     case 'pending':
       textStyle = '#E52B1A';
--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,26 +1,33 @@
 import { FC, memo } from 'react';
 
 import styles from './modal.module.css';
-
-import { CloseIcon } from '@zlden/react-developer-burger-ui-components';
 import { TModalUIProps } from './type';
 import { ModalOverlayUI } from '@ui';
 
+import { CloseIcon } from '@zlden/react-developer-burger-ui-components';
+
+// Компонент модального окна
 export const ModalUI: FC<TModalUIProps> = memo(
   ({ title, onClose, children }) => (
     <>
-      <div className={styles.modal}>
+      <div className={styles.modal} data-modal>
         <div className={styles.header}>
-          <h3 className={`${styles.title} text text_type_main-large`}>
+          <h3
+            className={
+              typeof title === 'string' && /^#?\d+$/.test(title.trim())
+                ? `${styles.title} text text_type_digits-medium`
+                : `${styles.title} text text_type_main-large`
+            }
+          >
             {title}
           </h3>
-          <button className={styles.button} type='button'>
+          <button className={styles.button} type='button' data-close>
             <CloseIcon type='primary' onClick={onClose} />
           </button>
         </div>
         <div className={styles.content}>{children}</div>
       </div>
-      <ModalOverlayUI onClick={onClose} />
+      <ModalOverlayUI onClick={onClose} data-modal-overlay />
     </>
   )
 );
