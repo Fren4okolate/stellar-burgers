4ece71af59ee5e91ee5736887da555f720980a54
Недбай Иван Дмитриевич <i.nedbai@MacBook-Pro-de-Nedbaj.local>
2026-04-27 19:17:17 +0300

    fix .env

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html'
     }),
-    new Dotenv()
+    new Dotenv({
+      path: './.env.example',
+      systemvars: true
+    })
   ],
   resolve: {
     extensions: [
