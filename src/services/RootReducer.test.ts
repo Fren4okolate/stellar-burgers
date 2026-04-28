98b3045ff2e43c48bafc4ca1a28bbc5793c88c88
Недбай Иван Дмитриевич <i.nedbai@MacBook-Pro-de-Nedbaj.local>
2026-04-28 14:54:36 +0300

    fix @@init'

--- src/services/RootReducer.test.ts
+++ src/services/RootReducer.test.ts
@@ -1,18 +1,19 @@
 import { rootReducer } from './RootReducer';
+import { constructorReducer } from './slices/constructor/constructor';
+import { feedReducer } from './slices/feed/feed';
+import { ingredientsReducer } from './slices/ingredients/ingredients';
+import { authReducer } from './slices/auth/auth';
 
 describe('rootReducer', () => {
-  it('инициализируется без ошибок', () => {
-    // Диспатчим экшен инициализации rootReducer
-    const state = rootReducer(undefined, { type: '@@INIT' });
-    // Проверяем, что state определён
-    expect(state).toBeDefined();
-    // Проверяем, что в state есть ключ ingredients
-    expect(state).toHaveProperty('ingredients');
-    // Проверяем, что в state есть ключ burgerConstructor
-    expect(state).toHaveProperty('burgerConstructor');
-    // Проверяем, что в state есть ключ auth
-    expect(state).toHaveProperty('auth');
-    // Проверяем, что в state есть ключ feed
-    expect(state).toHaveProperty('feed');
+  it('handles unknown action correctly', () => {
+    const initAction = { type: '@@init' };
+    const state = rootReducer(undefined, initAction);
+
+    expect(state).toEqual({
+      ingredients: ingredientsReducer(undefined, initAction),
+      burgerConstructor: constructorReducer(undefined, initAction),
+      auth: authReducer(undefined, initAction),
+      feed: feedReducer(undefined, initAction)
+    });
   });
 });
